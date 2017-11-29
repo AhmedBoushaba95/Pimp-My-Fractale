7a234426f8cd398d2568e5f6de62c1f7f9f72cb5
rouyer_o <rouyer_o@916c51f7-7e62-4f47-a287-cbda59804c34>
2017-11-29 22:48:11 +0000

    maj

--- Pimp My Fractale/verove_c/function.php
+++ Pimp My Fractale/verove_c/function.php
@@ -1,6 +1,10 @@
 <?php
 //if($_POST['nbr_iterations'])
 //$_POST['nbr_degres'];
+/*ini_set("display_errors", 1);
+ini_set("track_errors", 1);
+ini_set("html_errors", 1);
+error_reporting(E_ALL);*/
 $min_x=-2;
 $max_x=1;
 $min_y=-1;
@@ -40,24 +44,34 @@ for($x=0;$x<=$dim_x;$x++) {
     //echelle
     $c1=$min_x+($max_x-$min_x)/$dim_x*$x;
     $c2=$min_y+($max_y-$min_y)/$dim_y*$y;
-    $z1=0;
-    $z2=0;
-//$z1 partie reelle
-//$z2 partie imaginaire
-    for($i=0;$i<100;$i++) {
-      $new1=pow((($z1*$z1)+($z2*$z2)),(($n/2)*cos($n*atan2($z1,$z2))))+$c1; //xtmp=(x*x+y*y)^(n/2)*cos(n*atan2(y,x)) + a
-      $new2=pow((($z1*$z1)+($z2*$z2)),(($n/2)*sin($n*atan2($z1,$z2))))+$c2;
-      $z1=$new1;
-      $z2=$new2;
+    //$z1 partie reelle
+    //$z2 partie imaginaire
+    $z_r=0;
+    $z_i=0;
 
-      if($z1*$z1+$z2*$z2>=2) {
-        break;
+    for($i=0;$i<100;$i++) {
+      $new1=pow(($z_r*$z_r+$z_i*$z_i),($n/2))*cos($n*atan2($z_i,$z_r))+$c1; //xtmp=(x*x+y*y)^(n/2)*cos(n*atan2(y,x)) + a
+      $new2=pow(($z_r*$z_r+$z_i*$z_i),($n/2))*sin($n*atan2($z_i,$z_r))+$c2;
+      /*$new1=$z_r*$z_r-$z_i*$z_i+$c1; //xtmp=(x*x+y*y)^(n/2)*cos(n*atan2(y,x)) + a
+      $new2=2*$z_r*$z_i+$c2;*/
+      $z_r=$new1;
+      $z_i=$new2;
+
+      if($new1*$new1+$new2*$new2>=2) {//calcul de r carré du module
+        break; //on arrete la boucle
       }
     }
     if($i<20) { //ramification k
-      imagesetpixel ($im, $x, $y, $color);
+      imagesetpixel ($im, $x, $y, $color);//on pose un pixel aux coordonnées x ety
     }
+    /*if($x=($dim_x/2)){
+    imagesetpixel ($im, $x, 0, $color);
+  }*/
   }
+  /*if($y=($dim_y/2))
+  {
+  imagesetpixel ($im, 0, $y, $color);
+}*/
 }
 
 imagepng($im);
