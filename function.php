0686f758f01cb4f46cc18ac7197dfe7f36c243c2
rouyer_o <rouyer_o@916c51f7-7e62-4f47-a287-cbda59804c34>
2017-11-29 22:04:20 +0000

    maj

--- function.php
+++ function.php
@@ -5,18 +5,20 @@ $min_x=-2;
 $max_x=1;
 $min_y=-1;
 $max_y=1;
-
+$n=8;
+//taille image
 $dim_x=1100;
 $dim_y=900;
-
+//creation image
 $im = @imagecreate($dim_x, $dim_y)
   or die("Impossible de charger la blibliothèque gd");
 header("Content-Type: image/png");
+//couleur
 $black_color=imagecolorallocate($im, 0, 0, 0);
 
 if($_POST['noir'])
 {
-	$color = imagecolorallocate($im, 0, 0, 0); 
+	$color = imagecolorallocate($im, 0, 0, 0);
 }
 else if($_POST['bleu'])
 {
@@ -31,19 +33,23 @@ else if($_POST['rouge'])
 	$color = imagecolorallocate($im, 233, 56, 63);
 }
 
-
-for($y=0;$y<=$dim_y;$y++) {
-  for($x=0;$x<=$dim_x;$x++) {
+for($x=0;$x<=$dim_x;$x++) {
+  //mise a lechelle par rapport a limage en y
+  for($y=0;$y<=$dim_y;$y++) {
+  //mise a lechelle par rapport a limage en x
+    //echelle
     $c1=$min_x+($max_x-$min_x)/$dim_x*$x;
     $c2=$min_y+($max_y-$min_y)/$dim_y*$y;
     $z1=0;
     $z2=0;
-
+//$z1 partie reelle
+//$z2 partie imaginaire
     for($i=0;$i<100;$i++) {
-      $new1=$z1*$z1-$z2*$z2+$c1;
-      $new2=2*$z1*$z2+$c2;
+      $new1=pow((($z1*$z1)+($z2*$z2)),(($n/2)*cos($n*atan2($z1,$z2))))+$c1; //xtmp=(x*x+y*y)^(n/2)*cos(n*atan2(y,x)) + a
+      $new2=pow((($z1*$z1)+($z2*$z2)),(($n/2)*sin($n*atan2($z1,$z2))))+$c2;
       $z1=$new1;
       $z2=$new2;
+
       if($z1*$z1+$z2*$z2>=2) {
         break;
       }
@@ -56,3 +62,5 @@ for($y=0;$y<=$dim_y;$y++) {
 
 imagepng($im);
 imagedestroy($im);
+/*  $new1=pow((($z1*$z1)+($z2*$z2)),(($n/2)))*cos($n*atan2($z1,$z2))+$c1; //xtmp=(x*x+y*y)^(n/2)*cos(n*atan2(y,x)) + a
+  $new2=pow((($z1*$z1)+($z2*$z2)),(($n/2)))*sin($n*atan2($z1,$z2))+$c2;*/
