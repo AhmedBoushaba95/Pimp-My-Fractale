0c66120263b4f01164ca4bb825a2051936196224
rouyer_o <rouyer_o@916c51f7-7e62-4f47-a287-cbda59804c34>
2017-11-30 00:26:53 +0000

    maj

--- function.php
+++ function.php
@@ -9,7 +9,7 @@ $min_x=-2;
 $max_x=1;
 $min_y=-1;
 $max_y=1;
-$n=8;
+$n=2;
 //taille image
 $dim_x=1100;
 $dim_y=900;
@@ -37,9 +37,9 @@ else if($_POST['rouge'])
 	$color = imagecolorallocate($im, 233, 56, 63);
 }
 
-for($x=0;$x<=$dim_x;$x++) {
+for($x=0;$x<=$dim_x;$x++){
   //mise a lechelle par rapport a limage en y
-  for($y=0;$y<=$dim_y;$y++) {
+  for($y=0;$y<=$dim_y;$y++){
   //mise a lechelle par rapport a limage en x
     //echelle
     $c1=$min_x+($max_x-$min_x)/$dim_x*$x;
@@ -48,30 +48,22 @@ for($x=0;$x<=$dim_x;$x++) {
     //$z2 partie imaginaire
     $z_r=0;
     $z_i=0;
+    $i= 0;
+           while ($z_r*$z_r + $z_i*$z_i < 4 AND $i < $n)
+           {
+               $mod = sqrt(($z_r * $z_r) + ($z_i * $z_i));
+               $arg = atan2($z_i, $z_r);
+               $z_r = pow($mod, 20) * cos(20 * $arg) + $c_r;
+               $z_i =  pow($mod, 20) * sin(20 * $arg) + $c_i;
+               $i++;
+           }
+           if ($i == $n)
+               imagesetpixel($image, $x, $y, $color);
+           else
+               imagesetpixel($image, $x, $y, $color);
+       }
 
-    for($i=0;$i<100;$i++) {
-      $new1=pow(($z_r*$z_r+$z_i*$z_i),($n/2))*cos($n*atan2($z_i,$z_r))+$c1; //xtmp=(x*x+y*y)^(n/2)*cos(n*atan2(y,x)) + a
-      $new2=pow(($z_r*$z_r+$z_i*$z_i),($n/2))*sin($n*atan2($z_i,$z_r))+$c2;
-      /*$new1=$z_r*$z_r-$z_i*$z_i+$c1; //xtmp=(x*x+y*y)^(n/2)*cos(n*atan2(y,x)) + a
-      $new2=2*$z_r*$z_i+$c2;*/
-      $z_r=$new1;
-      $z_i=$new2;
-
-      if($new1*$new1+$new2*$new2>=2) {//calcul de r carré du module
-        break; //on arrete la boucle
-      }
-    }
-    if($i<20) { //ramification k
-      imagesetpixel ($im, $x, $y, $color);//on pose un pixel aux coordonnées x ety
-    }
-    /*if($x=($dim_x/2)){
-    imagesetpixel ($im, $x, 0, $color);
-  }*/
   }
-  /*if($y=($dim_y/2))
-  {
-  imagesetpixel ($im, 0, $y, $color);
-}*/
 }
 
 imagepng($im);
