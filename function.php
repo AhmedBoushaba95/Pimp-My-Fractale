1e79d5328c32ea89185746dfa8b35e4c4a32d98b
rouyer_o <rouyer_o@916c51f7-7e62-4f47-a287-cbda59804c34>
2017-11-30 10:49:39 +0000

    fracales

--- function.php
+++ function.php
@@ -1,72 +1,75 @@
 <?php
-//if($_POST['nbr_iterations'])
-//$_POST['nbr_degres'];
-/*ini_set("display_errors", 1);
-ini_set("track_errors", 1);
-ini_set("html_errors", 1);
-error_reporting(E_ALL);*/
-$min_x=-2;
-$max_x=1;
-$min_y=-1;
-$max_y=1;
-$n=2;
-//taille image
-$dim_x=1100;
-$dim_y=900;
-//creation image
-$im = @imagecreate($dim_x, $dim_y)
-  or die("Impossible de charger la blibliothèque gd");
-header("Content-Type: image/png");
+$x1 = -2.1;
+$x2 = 0.6;
+$y1 = -1.2;
+$y2 = 1.2;
+$zoom = 100;
+if(empty($_POST['nbr_iterations']))
+{
+$iterations_max =2;
+}else
+{
+$iterations_max = $_POST['nbr_iterations'];
+}
+if(empty($_POST['nbr_degres']))
+{
+$n=50;
+}else
+{
+$n=$_POST['nbr_degres'];
+}
+
+$image_x = ($x2 - $x1)*$zoom;
+$image_y = ($y2 - $y1)*$zoom;
 //couleur
 $black_color=imagecolorallocate($im, 0, 0, 0);
 
-if($_POST['noir'])
+if(!empty($_POST['bleu']))
 {
-	$color = imagecolorallocate($im, 0, 0, 0);
+ $color = imagecolorallocate($image, 53, 122, 183);
 }
-else if($_POST['bleu'])
+else if(!empty($_POST['vert']))
 {
-	$color = imagecolorallocate($im, 53, 122, 183);
+  $color = imagecolorallocate($image, 31, 160, 85);
 }
-else if($_POST['vert'])
+else if(!empty($_POST['rouge']))
 {
-	$color = imagecolorallocate($im, 31, 160, 85);
+     $color = imagecolorallocate($image, 233, 56, 63);
 }
-else if($_POST['rouge'])
+else
 {
-	$color = imagecolorallocate($im, 233, 56, 63);
+$color=imagecolorallocate($image, 255, 255, 255);
 }
 
-for($x=0;$x<=$dim_x;$x++){
-  //mise a lechelle par rapport a limage en y
-  for($y=0;$y<=$dim_y;$y++){
-  //mise a lechelle par rapport a limage en x
-    //echelle
-    $c1=$min_x+($max_x-$min_x)/$dim_x*$x;
-    $c2=$min_y+($max_y-$min_y)/$dim_y*$y;
-    //$z1 partie reelle
-    //$z2 partie imaginaire
-    $z_r=0;
-    $z_i=0;
-    $i= 0;
-           while ($z_r*$z_r + $z_i*$z_i >= 4 AND $i < 100)
-           {
-               $mod = sqrt(($z_r * $z_r) + ($z_i * $z_i));
-               $arg = atan2($z_i, $z_r);
-               $z_r = pow($mod, $n) * cos($n * $arg) + $c1;
-               $z_i = pow($mod, $n) * sin($n * $arg) + $c2;
-               $i++;
-           }
-           if ($i<100)
-               imagesetpixel($im, $x, $y, $olor);
-           else
-               imagesetpixel($im, $x, $y, $black_color);
-       }
+$image = imagecreatetruecolor($image_x, $image_y);
+$white = imagecolorallocate($image, 255, 255, 255);
+$noir  = imagecolorallocate($image, 0, 0, 0);
+imagefill($image, 0 ,0 , $white);
 
-  }
+$debut = microtime(true);
+for($x = 0; $x < $image_x; $x++){
+    for($y = 0; $y < $image_y; $y++){
+            $c_r = $x/$zoom+$x1;
+            $c_i = $y/$zoom+$y1;
+            $z_r = 0;
+            $z_i = 0;
+            $i   = 0;
+        do{
+  $mod = sqrt(($z_r * $z_r) + ($z_i * $z_i));
+  $arg = atan2($z_i, $z_r);
+  $z_r = pow($mod, $n) * cos($n * $arg) + $c_r;
+  $z_i = pow($mod, $n) * sin($n * $arg) + $c_i;
+  $i++;
+        }
+while($z_r*$z_r + $z_i*$z_i < 4 AND $i < $iterations_max);
+        if($i == $iterations_max)
+	            imagesetpixel($image, $x, $y, $noir);
+		        }
+			}
 
+$temps = round(microtime(true) - $debut, 3);
 
-imagepng($im);
-imagedestroy($im);
-/*  $new1=pow((($z1*$z1)+($z2*$z2)),(($n/2)))*cos($n*atan2($z1,$z2))+$c1; //xtmp=(x*x+y*y)^(n/2)*cos(n*atan2(y,x)) + a
-  $new2=pow((($z1*$z1)+($z2*$z2)),(($n/2)))*sin($n*atan2($z1,$z2))+$c2;*/
+imagestring($image, 3, 1, 1, $temps, $noir);
+
+header('Content-type: image/png');
+imagepng($image);
